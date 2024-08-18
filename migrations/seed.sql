d8eb95a39091e37076747b9bce6c9003a3a5eedf
olzzhas <olzhasayato@gmail.com>
2024-08-18 20:28:10 +0500

    docker migrate / seed

---
+++ migrations/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (email, name, lastname, password_hash, role, image_url, additional_information, course, major, degree, faculty) VALUES
+   ('testuser1@example.com', 'Test', 'User1', 'hashed_password_1', 'STUDENT', 'test', 'test', 4, 'test', 'test', 'test'),
+   ('testuser2@example.com', 'Test', 'User2', 'hashed_password_2', 'STUDENT', 'test', 'test', 4, 'test', 'test', 'test');
+
+
